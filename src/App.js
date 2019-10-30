2a600dd0f639d93008edc86451d3804b1ee3fcf6
rungokarol <karolrungo@gmail.com>
2019-10-30 22:14:57 +0100

    asic tests for App- to be refactored

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 import PrivateRoute from './hoc/PrivateRoute';
 import Login from './components/Login';
 import Home from './components/Home';
@@ -9,13 +9,13 @@ import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
         <Switch>
           <Route path="/login" component={Login} />
           <PrivateRoute exact path="/" component={Home} />
           <Route render={() => <Redirect to="/" />} />
         </Switch>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />)
 });
+
+it('contains a div with App class', () => {
+  const app = shallow(<App />)
+  expect(app.find('.App')).toBeTruthy()
+})
+
+it('renders BrowserRouter with Switch', () => {
+  const app = shallow(<App />)
+  console.log(app.debug())
+  expect(app.find('BrowserRouter').length).toEqual(1)
+  expect(app.find('BrowserRouter').length).toEqual(1)
+})
