60200bc34b267f367a5caa2e862e23f2a8ca94aa
rungo <karol.rungo@nokia.com>
2019-11-07 11:22:56 +0100

    Updates

---
+++ precache-manifest.814080f16f260acd49fce8697f2a2738.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c01867aa657afa8aead887af01b09a7",
+    "url": "/spotify-api/index.html"
+  },
+  {
+    "revision": "3b9e94daf6acb69e0d68",
+    "url": "/spotify-api/static/css/main.7515d71a.chunk.css"
+  },
+  {
+    "revision": "9e50466ae7b20f35129f",
+    "url": "/spotify-api/static/js/2.e00dd36d.chunk.js"
+  },
+  {
+    "revision": "3b9e94daf6acb69e0d68",
+    "url": "/spotify-api/static/js/main.1a9cb07e.chunk.js"
+  },
+  {
+    "revision": "047575dd2489face92a6",
+    "url": "/spotify-api/static/js/runtime-main.6f59ff18.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/spotify-api/static/media/logo.25bf045c.svg"
+  }
+]);
