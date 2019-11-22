888d0106ca913720cf56318772752d9b4ed09274
rungo <karol.rungo@nokia.com>
2019-11-22 13:45:30 +0100

    hide navigation when user is not logged in

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './App.css';
 
 import {Route, Redirect, Switch} from 'react-router-dom';
 import PrivateRoute from './hoc/PrivateRoute';
+import AuthContext from './context/AuthContext'
 import Navigation from './UI/Navigation';
 import Login from './components/Login';
 import User from './components/User/User';
 import Playlists from './components/Playlists';
 import Home from './components/Home';
+
 
 import withSpotify from './hoc/withSpotify'
 
@@ -15,9 +17,10 @@ function App() {
   const HomeWithSpotify = withSpotify(Home)
   const PlaylistsWithSpotify = withSpotify(Playlists)
   const UserWithSpotify = withSpotify(User)
+  const {isAuthenticated} = useContext(AuthContext)
   return (
     <div className="App">
-      <Navigation />
+      { isAuthenticated()? <Navigation /> : null}
       <Switch>
         <Route path="/login" component={Login} />
         <PrivateRoute exact path="/" component={HomeWithSpotify} />
