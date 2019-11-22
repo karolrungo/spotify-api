85e15766fea9f1b3811848f88ea3c8a9d4841c5c
rungo <karol.rungo@nokia.com>
2019-11-22 16:08:34 +0100

    auth UTs refactored

--- src/App.test.js
+++ src/App.test.js
@@ -17,10 +17,6 @@ describe('<App />', () => {
     expect(app.find('.App')).toHaveLength(1);
   });
 
-  it('renders navigation', () => {
-    expect(app.find('Navigation')).toHaveLength(1);
-  });
-
   it('renders routing components', () => {
     expect(app.find(Switch)).toHaveLength(1);
     expect(app.find(Route)).toHaveLength(2);
@@ -28,33 +24,57 @@ describe('<App />', () => {
   });
 });
 
-describe('when rendering default path, <App />', () => {
+describe('when not logged in <App />', () => {
   const authContext = {
     isAuthenticated: jest.fn(),
   };
-  let app = null;
+  let wrapper = null;
 
   beforeEach(() => {
-    app = (
+    authContext.isAuthenticated.mockReturnValue(false);
+    const app = (
       <AuthContext.Provider value={authContext}>
         <MemoryRouter initialEntries={['/']}>
           <App />
         </MemoryRouter>
       </AuthContext.Provider>
     );
+    wrapper = mount(app)
   });
 
-  it('renders Home component when user is logged in', () => {
-    authContext.isAuthenticated.mockReturnValue(true);
-    const wrapper = mount(app);
+  it('renders Login component', () => {
+    expect(wrapper.find('Login').length).toEqual(1);
+  });
 
+  it('does not render Navigation component', () => {
+    expect(wrapper.find('Navigation').length).toEqual(0);
+  });
+});
+
+describe('when logged in <App />', () => {
+  const authContext = {
+    isAuthenticated: jest.fn(),
+  };
+  let wrapper = null;
+
+  beforeEach(() => {
+    authContext.isAuthenticated.mockReturnValue(true);
+    const app = (
+      <AuthContext.Provider value={authContext}>
+        <MemoryRouter initialEntries={['/']}>
+          <App />
+        </MemoryRouter>
+      </AuthContext.Provider>
+    );
+    wrapper = mount(app)
+  });
+
+  it('renders Home component by default', () => {
     expect(wrapper.find('Home').length).toEqual(1);
   });
 
-  it('renders Login component when user is not logged in', () => {
-    authContext.isAuthenticated.mockReturnValue(false);
-    const wrapper = mount(app);
-
-    expect(wrapper.find('Login').length).toEqual(1);
+  it('renders Navigation component', () => {
+    expect(wrapper.find('Navigation').length).toEqual(1);
   });
 });
+
